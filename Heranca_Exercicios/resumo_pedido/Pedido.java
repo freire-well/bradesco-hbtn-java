6230abce0d78163c5aa0fae3d27f43a4cc398afc
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-07-11 18:55:02 -0300

    FIX total sum

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -22,14 +22,14 @@ public class Pedido {
 
         System.out.println("------- RESUMO PEDIDO -------");
         for (ItemPedido item : itens) {
-            totalProdutos += item.getProduto().obterPrecoLiquido();
-            System.out.printf("Tipo: %s Titulo: %s Preco: %.2f Quant: %d Total: %.2f\n",
-                    item.getProduto().getClass()    .getSimpleName(), item.getProduto().getTitulo(),
+            totalProdutos += item.getProduto().obterPrecoLiquido() * item.getQuantidade();
+            System.out.printf("Tipo: %s  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n",
+                    item.getProduto().getClass().getSimpleName(), item.getProduto().getTitulo(),
                     item.getProduto().obterPrecoLiquido(), item.getQuantidade(),
-                    calcularTotal());
+                    item.getProduto().obterPrecoLiquido() * item.getQuantidade());
         }
-        totalPedido = totalProdutos * totalProdutos * (percentualDesconto / 100 );
         desconto = totalProdutos * (percentualDesconto / 100 );
+        totalPedido = totalProdutos - desconto;
         System.out.println("----------------------------");
         System.out.printf("DESCONTO: %.2f\n", desconto);
         System.out.printf("TOTAL PRODUTOS: %.2f\n", totalProdutos);
