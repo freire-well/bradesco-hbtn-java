f5f35a80bd9e3a21fd8fbe8de77884572a51d528
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-07-13 13:59:15 -0300

    fix exceptions treatment

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -1,5 +1,6 @@
+import exceptions.*;
 public class LivroEdicaoOuro extends Livro {
-    public LivroEdicaoOuro(String titulo, String autor, double preco) {
+    public LivroEdicaoOuro(String titulo, String autor, double preco) throws LivroInvalidoException, AutorInvalidoException {
         super(titulo, autor, preco);
     }
 
