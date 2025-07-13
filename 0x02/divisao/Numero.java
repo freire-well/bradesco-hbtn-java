5599c1440337e1e0e7ec3f13e2409403d3581463
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-07-13 12:17:11 -0300

    add divisao

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,17 @@
+public class Numero{
+    public static void dividir(int a, int b){
+
+        int resultado = 0;
+        try{
+            resultado = a/b;
+        } catch (java.lang.Exception e) {
+            resultado = 0;
+            System.out.printf("Nao eh possivel dividir por zero\n");
+        }finally {
+            System.out.printf("%d / %d = %d", a, b, resultado);
+
+        }
+    }
+
+}
+
