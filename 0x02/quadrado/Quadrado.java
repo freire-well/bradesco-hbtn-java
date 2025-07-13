f1081d189682a83a303865373297d40f751ad014
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-07-13 12:22:07 -0300

    add quadrado[

---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,10 @@
+public class Quadrado {
+    public static double area(double lado) throws java.lang.IllegalArgumentException{
+        if(lado < 0){
+            throw new java.lang.IllegalArgumentException("Lado deve possuir valor positivo");
+        }else{
+            return lado * lado;
+        }
+    }
+
+}
