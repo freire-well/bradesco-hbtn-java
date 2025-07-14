29ce86d9772bd1bbb1f1f95878a6123285927fc4
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-07-14 12:17:15 +0000

    apply logic

--- arquivos/0/FileWritingExercise.java
+++ arquivos/0/FileWritingExercise.java
@@ -1,7 +1,10 @@
 import java.io.BufferedWriter;
- import java.io.FileWriter;
+import java.io.File;
+import java.io.FileWriter;
  import java.io.IOException;
- import java.util.Scanner;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.util.Scanner;
 
 
 public class FileWritingExercise{
@@ -10,15 +13,23 @@ public class FileWritingExercise{
         // Solicita o nome do arquivo
         System.out.print("Digite o nome do arquivo (com extensão .txt): ");
         String fileName = scanner.nextLine();
-        System.out.print("Digite o conteudo do arquivo linha por linha (para parar, digite \"sair\"): ");
-        String linha;
-        while(true){
-            linha = scanner.nextLine();
-            if(linha.equals("sair")){
-                break;
+        try {
+            System.out.print("Digite o conteudo do arquivo linha por linha (para parar, digite \"sair\"): ");
+            String linha;
+            File file = new File(fileName);
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            while(true){
+                linha = scanner.nextLine();
+                if(linha.equals("sair")){
+                    break;
+                }
+                writer.write(linha);          
             }
+            writer.close();
+        } catch (IOException e) {
+         System.out.println(e.getMessage());
         }
-        
 
 
 
