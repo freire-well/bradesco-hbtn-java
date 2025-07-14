da34097be231ae41bd69d09c765d58b73de9a35f
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-07-14 12:43:16 +0000

    add arquivos

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+    
+        Path path = Paths.get("./exemplo.txt");
+        try{
+        List<String> lines = Files.readAllLines(path);
+        System.out.println("Conteúdo do arquivo 'exemplo.txt':");
+        System.out.println("");
+        for (String line : lines) {
+            System.out.println(line);
+        }
+        System.out.println("");
+        System.out.println("Leitura do arquivo concluída.");
+        }catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
