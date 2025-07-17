497207d4ce3191c8731205bddc45145aea05891e
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-07-17 20:32:34 -0300

    add collections_Exercicios

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Blog {
+    List<Post> postagens;
+
+    public Blog() {
+        this.postagens = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post postagem){
+        postagens.add(postagem);
+    }
+
+    public Set<String> obterTodosAutores(){
+        ArrayList<String> temp = new ArrayList();
+        for (int i = 0; i < postagens.size(); i++) {
+            temp.add(postagens.get(i).getAutor());
+        }
+        Collections.sort(temp);
+        return new HashSet<>(temp);
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria(){
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < postagens.size(); i++) {
+            String categoriaLista = postagens.get(i).getCategoria();
+            map.merge(categoriaLista, 1, Integer::sum);
+        }
+
+        return map;
+    }
+}
