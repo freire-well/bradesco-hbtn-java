e7d77aa9aef6f03e4992e2ea46bef3ab4a1cb13a
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-08-01 15:07:32 -0300

    add streams colector

--- Streams_Collectors_Optional/livros/Produto.java
+++ Streams_Collectors_Optional/livros/Produto.java
@@ -29,7 +29,7 @@ public class Produto {
 
     @Override
     public String toString() {
-        return String.format("[%d] %s %s %.2f", codigo, nome, categoria, preco);
+        return String.format("[%d] %s %s R$ %.2f", codigo, nome, categoria, preco);
 
 
     }
