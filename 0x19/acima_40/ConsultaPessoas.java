14f684f4f04fcb453d40622fd7060ccfcf0ad5fb
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-08-05 13:40:27 -0300

    add 40+

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+public class ConsultaPessoas {
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoasList  ){
+        return pessoasList.stream().collect(
+                Collectors.groupingBy(
+                        Pessoa::getCargo));
+    }
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoasList  ){
+        return pessoasList.stream().collect(
+                Collectors.groupingBy(
+                        Pessoa::getCargo, Collectors.mapping(Pessoa::getIdade, toList())));
+    }
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoasList  ){
+        return pessoasList.stream().collect(
+                Collectors.groupingBy(
+                        Pessoa::getCargo, Collectors.filtering((pessoa -> pessoa.getIdade() > 40), toList())));
+    }
+}
