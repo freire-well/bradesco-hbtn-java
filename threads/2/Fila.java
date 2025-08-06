5a12e6a2a8ea9874ddc55999dc2fa095c6e253da
Wellington Freire <wellington.freire@live.com>
2025-08-06 12:49:01 -0300

    add threads

--- threads/2/Fila.java
+++ threads/2/Fila.java
@@ -2,15 +2,15 @@ import java.util.LinkedList;
 
 public class Fila {
     private LinkedList<Integer> fila;
-    private final int tamanho;
+    private final int capacidade;
 
     public Fila( int tamanho) {
         this.fila = new LinkedList<>();
-        this.tamanho = tamanho;
+        this.capacidade = tamanho;
     }
 
     public synchronized void adicionar(int i) throws InterruptedException {
-        if(fila.size() == tamanho){
+        if(fila.size() == capacidade){
             wait();
         }else{
             this.fila.add(i);
