c12f286b1aaf57ec1e42274b3345856cbd787c59
Wellington Freire <wellington.f.silva@bradesco.com.br>
2025-10-31 18:27:08 -0300

    ddd

--- ddd/0/application/PedidoService.java
+++ ddd/0/application/PedidoService.java
@@ -1,13 +1,12 @@
 package application;
 
-import domain.Cliente;
 import domain.Pedido;
 import infrastructure.PedidoRepository;
 
 public class PedidoService {
-    PedidoRepository rep = new PedidoRepository();
+    PedidoRepository pedidoRepository = new PedidoRepository();
 
     public void save(Pedido pedido){
-        rep.salvar(pedido);
+        pedidoRepository.salvar(pedido);
     }
 }
